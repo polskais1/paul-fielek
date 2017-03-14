5de69b0e7c4ac670c2993cf8ec452151724a18c2
Paul Fielek <paulfielek@gmail.com>
2017-03-14 19:08:58 -0400

    added some more styles

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,3 +1,13 @@
 $(function() {
+  repositionName();
 
+  $(window).scroll(function(e) {
+    repositionName();
+  });
+
+  function repositionName() {
+    $name = $('#name');
+    // Center the name
+    $name.css('bottom', ($(document).height() / 2) - ($name.height() / 2));
+  }
 });
